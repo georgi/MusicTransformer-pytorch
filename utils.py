d537560852310ac316f2d71ad3722141f4533963
Matthias Georgi <mmg@fb.com>
2021-02-28 23:00:53 +0100

    nucelus sampling

--- utils.py
+++ utils.py
@@ -17,7 +17,7 @@ def find_files_by_extensions(root, exts=[]):
                 yield os.path.join(path, name)
 
 
-def sample(model, sample_length, prime_sequence, device, temperature=1, k=10):
+def sample(model, sample_length, prime_sequence, device, top_p, top_k=10):
     import torch
     import torch.nn.functional as F
     model.eval()
@@ -25,12 +25,14 @@ def sample(model, sample_length, prime_sequence, device, temperature=1, k=10):
     input_tensor = torch.LongTensor(input_sequence).unsqueeze(0).to(device)
 
     for i in tqdm(range(sample_length)):
-        out = model(input_tensor)[0, -1, :]
-        probs = F.softmax(out / temperature, dim=0)
-        top = torch.topk(probs, k)
-        for i in range(len(probs)):
-            if i not in top.indices:
-                probs[i] = 0
+        logits = model(input_tensor)[0, -1, :]
+        probs = F.softmax(logits, dim=-1)
+        sorted_probs, sorted_indices = torch.sort(probs, descending=True)
+        to_remove = torch.cumsum(sorted_probs, dim=-1) > top_p
+        to_remove[0] = False  # always incude top result
+        to_remove[top_k:] = True  # at most include top K results
+        indices_to_remove = sorted_indices[to_remove]
+        probs.scatter_(0, indices_to_remove, 0.0)
         c = torch.multinomial(probs, 1)
         input_tensor = torch.cat([input_tensor[:, 1:], c[None]], dim=1)
         input_sequence.append(c.item())
