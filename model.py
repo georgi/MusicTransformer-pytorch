c85826881d1aa83987e1a80a7ee85e7e0a1da50e
Matthias Georgi <mmg@fb.com>
2021-03-04 07:04:18 +0100

    use transformer encoder directly

--- model.py
+++ model.py
@@ -39,53 +39,36 @@ class TransformerModel(nn.Module):
     ):
         super(TransformerModel, self).__init__()
         self.model_type = 'Transformer'
+        self.d_model = d_model
         self.sequence_len = sequence_len
         self.embedding = nn.Embedding(vocab_size, d_model)
         self.pos_encoder = PositionalEncoding(d_model, dropout, sequence_len)
-        self.transformer = nn.Transformer(
-            d_model=d_model,
-            nhead=nhead,
-            num_encoder_layers=num_layers,
-            num_decoder_layers=0,
-            dropout=dropout,
-            dim_feedforward=dim_feedforward,
-            custom_decoder=DummyDecoder()
-        )
+        encoder_norm = nn.LayerNorm(d_model)
+        encoder_layers = nn.TransformerEncoderLayer(d_model, nhead, dim_feedforward, dropout)
+        self.transformer_encoder = nn.TransformerEncoder(encoder_layers, num_layers, encoder_norm)
         self.decoder = nn.Linear(d_model, vocab_size)
         self.init_weights()
 
     def gen_mask(self, size):
-        return self.transformer.generate_square_subsequent_mask(size)
+        mask = (torch.triu(torch.ones(size, size)) == 1).transpose(0, 1)
+        mask = mask.float().masked_fill(mask == 0, float('-inf')).masked_fill(mask == 1, float(0.0))
+        return mask
 
     def init_weights(self):
         initrange = 0.1
         self.decoder.bias.data.zero_()
         self.decoder.weight.data.uniform_(-initrange, initrange)
+        self.embedding.weight.data.uniform_(-initrange, initrange)
 
     def forward(self, src, mask=None):
         if mask is None:
             device = next(self.parameters()).device
             mask = self.gen_mask(src.shape[1]).to(device)
-        src = self.embedding(src)
+        src = self.embedding(src) * math.sqrt(self.d_model)
         src = src.permute(1, 0, 2)  # (max_seq, batch_size, d_model)
         src = self.pos_encoder(src)
-        out = self.transformer(src=src, tgt=src, src_mask=mask)
+        out = self.transformer_encoder(src=src, src_mask=mask)
         out = out.permute(1, 0, 2)  # (batch_size, max_seq, d_model)
         out = self.decoder(out)
         return out
 
-
-class DummyEncoder(nn.Module):
-    def __init__(self):
-        super(DummyEncoder, self).__init__()
-
-    def forward(self, src, *args, **kwargs):
-        return src
-
-
-class DummyDecoder(nn.Module):
-    def __init__(self):
-        super(DummyDecoder, self).__init__()
-
-    def forward(self, tgt, memory, *args, **kwargs):
-        return memory
